ac7dab08d2a9337c2429654558f50e06720281ea
Jonas Johansson <jonas.johansson@vxa.se>
2021-03-15 17:09:21 +0100

    Cleanup

--- LoopiaDDNSApp.App/Domain/Services/DNSService.cs
+++ LoopiaDDNSApp.App/Domain/Services/DNSService.cs
@@ -11,8 +11,8 @@ namespace LoopiaDDNSApp.Domain.Services
 {
     public class DNSService : IDNSService
     {
-        private readonly Config _config;
         private readonly IIpifyRepository _ipRepo;
+        private readonly Config _config;
 
         public DNSService(IIpifyRepository ipRepo, IOptions<Config> options)
         {
@@ -22,6 +22,7 @@ namespace LoopiaDDNSApp.Domain.Services
 
         public async Task<bool> UpdateDNS()
         {
+
             // Get external IP
             var ip = await _ipRepo.GetExternalIp();
 
@@ -29,25 +30,25 @@ namespace LoopiaDDNSApp.Domain.Services
             var username = _config.Username;
             var password = _config.Password;
             var domain = _config.Domain;
-            var subDomain = _config.Subdomain;
+            var subdomain = _config.Subdomain;
             var ttl = _config.ttl;
 
             // Build XML RPC Client
             var proxy = XmlRpcProxyGen.Create<ILoopiaServiceProxy>();
             proxy.Url = _config.LoopiaAPIUri;
+            var record = new RecordDto();
 
-            // Contact LoopiaAPI, grab a list of all your domainRecords for this specific domain.
+            // Contact Loopia, grab a list of all your domainRecords
             try
             {
-                var result = proxy.GetZoneRecords(username, password, domain, subDomain);
+                var result = proxy.GetZoneRecords(username, password, domain, subdomain); 
 
-                // We select the first one, which ought to be your IP. TODO: Make actual logic?
-                var record = result.First();
+                // We select the first one, which ought to be your IP. TODO: Make actual logic.
+                record = result.First();
 
-                if (ip == record.rdata)
+                
+               if (ip == record.rdata)
                 {
-                    var currentTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
-                    Console.WriteLine(currentTime + "\nNo need to update\n Your current external ip: " + ip + "\n Your current Loopia DNS IP: " + record.rdata);
                     return true;
                 }
 
@@ -62,25 +63,10 @@ namespace LoopiaDDNSApp.Domain.Services
                 };
 
                 // Update
-                var status = proxy.UpdateZoneRecord(username, password, domain, subDomain, domainRecord);
-                var timeNow = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                var status = proxy.UpdateZoneRecord(username, password, domain, subdomain, domainRecord);
 
                 // act on status
 
-                {
-                    case "OK":
-                        Console.WriteLine("Updated" + timeNow);
-                        break;
-                    case "AUTH_ERROR":
-                        Console.WriteLine(timeNow + "\nAUTH_ERROR, Wrong username or password");
-                        break;
-                    case "BAD_INDATA":
-                        Console.WriteLine(timeNow + "\nBAD_INDATA, Incorrect configuration in config.json");
-                        break;
-                    default:
-                        Console.WriteLine(timeNow + "\nUnknown status reply from Loopia...");
-                        break;
-                }
             }
             catch (Exception e)
             {
--- LoopiaDDNSApp.App/Program.cs
+++ LoopiaDDNSApp.App/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using LoopiaDDNSApp.Domain.Services;
+﻿using LoopiaDDNSApp.Domain.Services;
 using LoopiaDDNSApp.Domain.Services.Interfaces;
 using LoopiaDDNSApp.InfraStructure.Managers;
 using LoopiaDDNSApp.InfraStructure.Managers.Interfaces;
@@ -28,29 +27,21 @@ namespace LoopiaDDNSApp
         {
             return Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
             {
-                try
-                { 
-                   var configuration = new ConfigurationBuilder().SetBasePath(hostContext.HostingEnvironment.ContentRootPath).AddJsonFile("config.json", false, true).AddEnvironmentVariables().Build();
-                   services.AddOptions();
-                   services.Configure<Config>(configuration.GetSection("Settings"));
+                var configuration = new ConfigurationBuilder().SetBasePath(hostContext.HostingEnvironment.ContentRootPath).AddJsonFile("config.json", false, true).AddEnvironmentVariables().Build();
 
-                   services.AddScoped<IHttpClientManager, HttpClientManager>();
-                   services.AddScoped<IIpifyRepository, IpifyRepository>();
-                   services.AddScoped<IDNSService, DNSService>();
-                   services.AddSingleton<IJobFactory, JobFactory>();
-                   services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
-                   services.AddSingleton<QuartzJobRunner>();
-                   services.AddScoped<UpdateDnsJob>();
-                   services.AddSingleton(new JobSchedule(typeof(UpdateDnsJob), configuration.GetValue<string>("Settings:Schedule")));
+                services.AddOptions();
+                services.Configure<Config>(configuration.GetSection("Settings"));
+                
+                services.AddScoped<IHttpClientManager, HttpClientManager>();
+                services.AddScoped<IIpifyRepository, IpifyRepository>();
+                services.AddScoped<IDNSService, DNSService>();
+                services.AddSingleton<IJobFactory, JobFactory>();
+                services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+                services.AddSingleton<QuartzJobRunner>();
+                services.AddScoped<UpdateDnsJob>();
+                services.AddSingleton(new JobSchedule(typeof(UpdateDnsJob), configuration.GetValue<string>("Settings:Schedule")));
 
-                   services.AddHostedService<QuartzHostedService>();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    Console.WriteLine("Is there a config.json available?");
-                    throw;
-                }
+                services.AddHostedService<QuartzHostedService>();
 
             }).UseWindowsService();
         }
